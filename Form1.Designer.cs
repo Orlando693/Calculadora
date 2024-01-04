60348fc8f655ff10e58ba1f3f83202dcdc857b7e
Orlando693 <Orlandosebastian693@gmail.com>
2024-01-03 22:22:00 -0400

    Comienzo del Proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace ProyectoCalculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BotonN7 = new System.Windows.Forms.Button();
+            this.BotonN4 = new System.Windows.Forms.Button();
+            this.BotonN1 = new System.Windows.Forms.Button();
+            this.BotonN0 = new System.Windows.Forms.Button();
+            this.BotonN8 = new System.Windows.Forms.Button();
+            this.BotonN5 = new System.Windows.Forms.Button();
+            this.BotonN2 = new System.Windows.Forms.Button();
+            this.BotonN9 = new System.Windows.Forms.Button();
+            this.BotonN6 = new System.Windows.Forms.Button();
+            this.BotonN3 = new System.Windows.Forms.Button();
+            this.BotonPunto = new System.Windows.Forms.Button();
+            this.BotonSuma = new System.Windows.Forms.Button();
+            this.BotonResta = new System.Windows.Forms.Button();
+            this.BotonMultiplicacion = new System.Windows.Forms.Button();
+            this.BotonDivision = new System.Windows.Forms.Button();
+            this.BotonLimpiar = new System.Windows.Forms.Button();
+            this.BotonBorrar = new System.Windows.Forms.Button();
+            this.BotonResultado = new System.Windows.Forms.Button();
+            this.Pantalla = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BotonN7
+            // 
+            this.BotonN7.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonN7.Location = new System.Drawing.Point(42, 145);
+            this.BotonN7.Name = "BotonN7";
+            this.BotonN7.Size = new System.Drawing.Size(55, 54);
+            this.BotonN7.TabIndex = 0;
+            this.BotonN7.Text = "7";
+            this.BotonN7.UseVisualStyleBackColor = true;
+            this.BotonN7.Click += new System.EventHandler(this.agregarnum);
+            // 
+            // BotonN4
+            // 
+            this.BotonN4.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonN4.Location = new System.Drawing.Point(42, 206);
+            this.BotonN4.Name = "BotonN4";
+            this.BotonN4.Size = new System.Drawing.Size(55, 54);
+            this.BotonN4.TabIndex = 1;
+            this.BotonN4.Text = "4";
+            this.BotonN4.UseVisualStyleBackColor = true;
+            this.BotonN4.Click += new System.EventHandler(this.agregarnum);
+            // 
+            // BotonN1
+            // 
+            this.BotonN1.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonN1.Location = new System.Drawing.Point(44, 267);
+            this.BotonN1.Name = "BotonN1";
+            this.BotonN1.Size = new System.Drawing.Size(55, 54);
+            this.BotonN1.TabIndex = 2;
+            this.BotonN1.Text = "1";
+            this.BotonN1.UseVisualStyleBackColor = true;
+            this.BotonN1.Click += new System.EventHandler(this.agregarnum);
+            // 
+            // BotonN0
+            // 
+            this.BotonN0.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonN0.Location = new System.Drawing.Point(42, 330);
+            this.BotonN0.Name = "BotonN0";
+            this.BotonN0.Size = new System.Drawing.Size(118, 54);
+            this.BotonN0.TabIndex = 3;
+            this.BotonN0.Text = "0";
+            this.BotonN0.UseVisualStyleBackColor = true;
+            this.BotonN0.Click += new System.EventHandler(this.agregarnum);
+            // 
+            // BotonN8
+            // 
+            this.BotonN8.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonN8.Location = new System.Drawing.Point(107, 145);
+            this.BotonN8.Name = "BotonN8";
+            this.BotonN8.Size = new System.Drawing.Size(55, 54);
+            this.BotonN8.TabIndex = 4;
+            this.BotonN8.Text = "8";
+            this.BotonN8.UseVisualStyleBackColor = true;
+            this.BotonN8.Click += new System.EventHandler(this.agregarnum);
+            // 
+            // BotonN5
+            // 
+            this.BotonN5.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonN5.Location = new System.Drawing.Point(107, 206);
+            this.BotonN5.Name = "BotonN5";
+            this.BotonN5.Size = new System.Drawing.Size(55, 54);
+            this.BotonN5.TabIndex = 5;
+            this.BotonN5.Text = "5";
+            this.BotonN5.UseVisualStyleBackColor = true;
+            this.BotonN5.Click += new System.EventHandler(this.agregarnum);
+            // 
+            // BotonN2
+            // 
+            this.BotonN2.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonN2.Location = new System.Drawing.Point(105, 267);
+            this.BotonN2.Name = "BotonN2";
+            this.BotonN2.Size = new System.Drawing.Size(55, 54);
+            this.BotonN2.TabIndex = 6;
+            this.BotonN2.Text = "2";
+            this.BotonN2.UseVisualStyleBackColor = true;
+            this.BotonN2.Click += new System.EventHandler(this.agregarnum);
+            // 
+            // BotonN9
+            // 
+            this.BotonN9.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonN9.Location = new System.Drawing.Point(168, 145);
+            this.BotonN9.Name = "BotonN9";
+            this.BotonN9.Size = new System.Drawing.Size(55, 54);
+            this.BotonN9.TabIndex = 7;
+            this.BotonN9.Text = "9";
+            this.BotonN9.UseVisualStyleBackColor = true;
+            this.BotonN9.Click += new System.EventHandler(this.agregarnum);
+            // 
+            // BotonN6
+            // 
+            this.BotonN6.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonN6.Location = new System.Drawing.Point(168, 206);
+            this.BotonN6.Name = "BotonN6";
+            this.BotonN6.Size = new System.Drawing.Size(55, 54);
+            this.BotonN6.TabIndex = 8;
+            this.BotonN6.Text = "6";
+            this.BotonN6.UseVisualStyleBackColor = true;
+            this.BotonN6.Click += new System.EventHandler(this.agregarnum);
+            // 
+            // BotonN3
+            // 
+            this.BotonN3.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonN3.Location = new System.Drawing.Point(168, 266);
+            this.BotonN3.Name = "BotonN3";
+            this.BotonN3.Size = new System.Drawing.Size(55, 54);
+            this.BotonN3.TabIndex = 9;
+            this.BotonN3.Text = "3";
+            this.BotonN3.UseVisualStyleBackColor = true;
+            this.BotonN3.Click += new System.EventHandler(this.agregarnum);
+            // 
+            // BotonPunto
+            // 
+            this.BotonPunto.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonPunto.Location = new System.Drawing.Point(168, 330);
+            this.BotonPunto.Name = "BotonPunto";
+            this.BotonPunto.Size = new System.Drawing.Size(55, 54);
+            this.BotonPunto.TabIndex = 10;
+            this.BotonPunto.Text = ".";
+            this.BotonPunto.UseVisualStyleBackColor = true;
+            this.BotonPunto.Click += new System.EventHandler(this.BotonPunto_Click);
+            // 
+            // BotonSuma
+            // 
+            this.BotonSuma.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonSuma.Location = new System.Drawing.Point(42, 84);
+            this.BotonSuma.Name = "BotonSuma";
+            this.BotonSuma.Size = new System.Drawing.Size(55, 54);
+            this.BotonSuma.TabIndex = 11;
+            this.BotonSuma.Text = "+";
+            this.BotonSuma.UseVisualStyleBackColor = true;
+            this.BotonSuma.Click += new System.EventHandler(this.BotonSuma_Click);
+            // 
+            // BotonResta
+            // 
+            this.BotonResta.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonResta.Location = new System.Drawing.Point(107, 84);
+            this.BotonResta.Name = "BotonResta";
+            this.BotonResta.Size = new System.Drawing.Size(55, 54);
+            this.BotonResta.TabIndex = 12;
+            this.BotonResta.Text = "-";
+            this.BotonResta.UseVisualStyleBackColor = true;
+            this.BotonResta.Click += new System.EventHandler(this.BotonResta_Click);
+            // 
+            // BotonMultiplicacion
+            // 
+            this.BotonMultiplicacion.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonMultiplicacion.Location = new System.Drawing.Point(168, 84);
+            this.BotonMultiplicacion.Name = "BotonMultiplicacion";
+            this.BotonMultiplicacion.Size = new System.Drawing.Size(55, 54);
+            this.BotonMultiplicacion.TabIndex = 13;
+            this.BotonMultiplicacion.Text = "*";
+            this.BotonMultiplicacion.UseVisualStyleBackColor = true;
+            this.BotonMultiplicacion.Click += new System.EventHandler(this.BotonMultiplicacion_Click);
+            // 
+            // BotonDivision
+            // 
+            this.BotonDivision.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonDivision.Location = new System.Drawing.Point(229, 84);
+            this.BotonDivision.Name = "BotonDivision";
+            this.BotonDivision.Size = new System.Drawing.Size(55, 54);
+            this.BotonDivision.TabIndex = 14;
+            this.BotonDivision.Text = "/";
+            this.BotonDivision.UseVisualStyleBackColor = true;
+            this.BotonDivision.Click += new System.EventHandler(this.BotonDivision_Click);
+            // 
+            // BotonLimpiar
+            // 
+            this.BotonLimpiar.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonLimpiar.Location = new System.Drawing.Point(229, 145);
+            this.BotonLimpiar.Name = "BotonLimpiar";
+            this.BotonLimpiar.Size = new System.Drawing.Size(55, 54);
+            this.BotonLimpiar.TabIndex = 15;
+            this.BotonLimpiar.Text = "CE";
+            this.BotonLimpiar.UseVisualStyleBackColor = true;
+            this.BotonLimpiar.Click += new System.EventHandler(this.BotonLimpiar_Click);
+            // 
+            // BotonBorrar
+            // 
+            this.BotonBorrar.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonBorrar.Location = new System.Drawing.Point(229, 206);
+            this.BotonBorrar.Name = "BotonBorrar";
+            this.BotonBorrar.Size = new System.Drawing.Size(55, 54);
+            this.BotonBorrar.TabIndex = 16;
+            this.BotonBorrar.Text = "←";
+            this.BotonBorrar.UseVisualStyleBackColor = true;
+            this.BotonBorrar.Click += new System.EventHandler(this.BotonBorrar_Click);
+            // 
+            // BotonResultado
+            // 
+            this.BotonResultado.Font = new System.Drawing.Font("Dubai", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonResultado.Location = new System.Drawing.Point(229, 267);
+            this.BotonResultado.Name = "BotonResultado";
+            this.BotonResultado.Size = new System.Drawing.Size(55, 117);
+            this.BotonResultado.TabIndex = 17;
+            this.BotonResultado.Text = "=";
+            this.BotonResultado.UseVisualStyleBackColor = true;
+            this.BotonResultado.Click += new System.EventHandler(this.BotonResultado_Click);
+            // 
+            // Pantalla
+            // 
+            this.Pantalla.Font = new System.Drawing.Font("Arial Narrow", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pantalla.Location = new System.Drawing.Point(44, 21);
+            this.Pantalla.Name = "Pantalla";
+            this.Pantalla.ReadOnly = true;
+            this.Pantalla.Size = new System.Drawing.Size(240, 57);
+            this.Pantalla.TabIndex = 18;
+            this.Pantalla.Text = "0";
+            this.Pantalla.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Pantalla.TextChanged += new System.EventHandler(this.Pantalla_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 414);
+            this.Controls.Add(this.Pantalla);
+            this.Controls.Add(this.BotonResultado);
+            this.Controls.Add(this.BotonBorrar);
+            this.Controls.Add(this.BotonLimpiar);
+            this.Controls.Add(this.BotonDivision);
+            this.Controls.Add(this.BotonMultiplicacion);
+            this.Controls.Add(this.BotonResta);
+            this.Controls.Add(this.BotonSuma);
+            this.Controls.Add(this.BotonPunto);
+            this.Controls.Add(this.BotonN3);
+            this.Controls.Add(this.BotonN6);
+            this.Controls.Add(this.BotonN9);
+            this.Controls.Add(this.BotonN2);
+            this.Controls.Add(this.BotonN5);
+            this.Controls.Add(this.BotonN8);
+            this.Controls.Add(this.BotonN0);
+            this.Controls.Add(this.BotonN1);
+            this.Controls.Add(this.BotonN4);
+            this.Controls.Add(this.BotonN7);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BotonN7;
+        private System.Windows.Forms.Button BotonN4;
+        private System.Windows.Forms.Button BotonN1;
+        private System.Windows.Forms.Button BotonN0;
+        private System.Windows.Forms.Button BotonN8;
+        private System.Windows.Forms.Button BotonN5;
+        private System.Windows.Forms.Button BotonN2;
+        private System.Windows.Forms.Button BotonN9;
+        private System.Windows.Forms.Button BotonN6;
+        private System.Windows.Forms.Button BotonN3;
+        private System.Windows.Forms.Button BotonPunto;
+        private System.Windows.Forms.Button BotonSuma;
+        private System.Windows.Forms.Button BotonResta;
+        private System.Windows.Forms.Button BotonMultiplicacion;
+        private System.Windows.Forms.Button BotonDivision;
+        private System.Windows.Forms.Button BotonLimpiar;
+        private System.Windows.Forms.Button BotonBorrar;
+        private System.Windows.Forms.Button BotonResultado;
+        private System.Windows.Forms.TextBox Pantalla;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCalculadora
+{
+    public partial class Form1 : Form
+    {
+        double primero;
+        double segundo;
+        string operador;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Clases.ClaseSuma obj1 = new Clases.ClaseSuma();
+        Clases.ClaseResta obj2 = new Clases.ClaseResta();
+        Clases.ClaseMultiplicacion obj3 = new Clases.ClaseMultiplicacion();
+        Clases.ClaseDivision obj4 = new Clases.ClaseDivision();
+
+        private void agregarnum(object sender, EventArgs e)
+        {
+            var boton = ((Button)sender);
+            if(Pantalla.Text == "0")
+            {
+                Pantalla.Text = "";
+
+            }
+            Pantalla.Text = Pantalla.Text + boton.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "7";
+        }
+
+        private void BotonN0_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "0";
+        }
+
+        private void BotonN1_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "1";
+        }
+
+        private void BotonN2_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "2";
+        }
+
+        private void BotonN3_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "3";
+        }
+
+        private void BotonN4_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "4";
+        }
+
+        private void BotonN5_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "5";
+        }
+
+        private void BotonN6_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "6";
+        }
+
+        private void BotonN8_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "8";
+        }
+
+        private void BotonN9_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + "9";
+        }
+
+        private void BotonPunto_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text = Pantalla.Text + ".";
+        }
+
+        private void BotonSuma_Click(object sender, EventArgs e)
+        {
+            operador = "+";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void BotonResta_Click(object sender, EventArgs e)
+        {
+            operador = "-";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void BotonMultiplicacion_Click(object sender, EventArgs e)
+        {
+            operador = "*";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void BotonDivision_Click(object sender, EventArgs e)
+        {
+            operador = "/";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void BotonResultado_Click(object sender, EventArgs e)
+        {
+            segundo = double.Parse(Pantalla.Text);
+
+            double Sum;
+            double Res;
+            double Mul;
+            double Div;
+
+            switch (operador)
+            {
+                case "+":
+                    Sum = obj1.Suma((primero), (segundo));
+                    Pantalla.Text = Sum.ToString();
+                    break;
+                case "-":
+                    Res = obj2.Resta((primero), (segundo));
+                    Pantalla.Text = Res.ToString();
+                    break;
+                case "*":
+                    Mul = obj3.Multiplicacion((primero), (segundo));
+                    Pantalla.Text = Mul.ToString();
+                    break;
+                case "/":
+                    Div = obj4.Division((primero), (segundo));
+                    Pantalla.Text = Div.ToString();
+                    break;
+            }
+        }
+
+        private void BotonLimpiar_Click(object sender, EventArgs e)
+        {
+            Pantalla.Clear();
+            Pantalla.Text = "0";
+        }
+
+        private void BotonBorrar_Click(object sender, EventArgs e)
+        {
+            if (Pantalla.Text.Length == 1)
+            {
+                Pantalla.Text = "0";
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text.Substring(0, Pantalla.Text.Length - 1);
+                
+            }
+        }
+
+        private void Pantalla_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
